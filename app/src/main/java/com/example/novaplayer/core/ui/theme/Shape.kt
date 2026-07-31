a98c11188f4e9fccab29509fb0be1ef8f8404a1d
Ali mahjoob <behnamuiux@gmail.com>
2026-07-31 22:42:01 +0330

    init

---
+++ app/src/main/java/com/example/novaplayer/core/ui/theme/Shape.kt
@@ -0,0 +1,19 @@
+package com.example.novaplayer.core.ui.theme
+
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+
+val ShapeSmall = RoundedCornerShape(4.dp)
+
+val ShapeMedium = RoundedCornerShape(12.dp)
+
+val ShapeLarge = RoundedCornerShape(16.dp)
+
+val ShapeExtraLarge = RoundedCornerShape(28.dp)
+
+
+val CardShape = RoundedCornerShape(16.dp)
+
+val ButtonShape = RoundedCornerShape(50)
--- app/src/main/java/com/example/novaplayer/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/novaplayer/musicplayer/MainActivity.kt
@@ -1,17 +1,16 @@
-package com.example.novaplayer
+package com.example.novaplayer.musicplayer
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.novaplayer.ui.theme.NovaPlayerTheme
+import com.example.novaplayer.core.ui.theme.NovaPlayerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +19,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             NovaPlayerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+
+
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NovaPlayerTheme {
-        Greeting("Android")
-    }
-}
