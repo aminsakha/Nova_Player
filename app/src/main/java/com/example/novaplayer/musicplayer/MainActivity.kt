9ec92711a15644926b5cda10c8c30fd73070e4d6
Ali mahjoob <behnamuiux@gmail.com>
2026-08-03 13:54:00 +0330

    init

--- app/src/main/java/com/example/novaplayer/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/novaplayer/musicplayer/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.example.novaplayer.core.media.presentation.screen.TestPlayer
 import com.example.novaplayer.core.ui.theme.NovaPlayerTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +23,18 @@ class MainActivity : ComponentActivity() {
             NovaPlayerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+
+                        TestPlayer()
+                    }
                 }
             }
         }
     }
 }
-
 
