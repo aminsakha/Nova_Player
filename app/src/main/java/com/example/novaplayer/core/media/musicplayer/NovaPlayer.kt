27ed6b69a7fc00bac40eb60f94fb8255718f4d73
Ali mahjoob <behnamuiux@gmail.com>
2026-08-03 12:39:15 +0330

    feat(media):add test msuic

---
+++ app/src/main/java/com/example/novaplayer/core/media/musicplayer/NovaPlayer.kt
@@ -0,0 +1,7 @@
+package com.example.novaplayer.core.media.musicplayer
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NovaPlayer: Application()
