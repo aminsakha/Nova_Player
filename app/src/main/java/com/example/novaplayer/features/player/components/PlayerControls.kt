5699c1a3423e755ed165467ed1813f834d866595
KALA hamrah <armannia.shayan@gmail.com>
2026-08-29 13:14:53 +0330

    next,previous icons changed

--- app/src/main/java/com/example/novaplayer/features/player/components/PlayerControls.kt
+++ app/src/main/java/com/example/novaplayer/features/player/components/PlayerControls.kt
@@ -1,17 +1,21 @@
 package com.example.novaplayer.features.player.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FastForward
-import androidx.compose.material.icons.filled.FastRewind
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.outlined.SkipNext
+import androidx.compose.material.icons.outlined.SkipPrevious
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -26,10 +30,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.ExperimentalMaterial3Api
 
 @Composable
 fun PlayerControls(
@@ -68,7 +68,7 @@ fun PlayerControls(
             ) {
                 Icon(
                     imageVector =
-                        Icons.Filled.FastRewind,
+                        Icons.Outlined.SkipPrevious,
                     contentDescription =
                         "Previous song",
                     modifier = Modifier.size(32.dp),
@@ -106,7 +106,7 @@ fun PlayerControls(
             ) {
                 Icon(
                     imageVector =
-                        Icons.Filled.FastForward,
+                        Icons.Outlined.SkipNext,
                     contentDescription =
                         "Next song",
                     modifier = Modifier.size(32.dp),
