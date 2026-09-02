a3d9793a68de6e5c9b84a90dd9ca2d24585cc894
Ali mahjoob <behnamuiux@gmail.com>
2026-09-03 01:39:07 +0330

    update ui and fix ui bugs

--- app/src/main/java/com/example/novaplayer/features/settings/presentation/screen/components/ThemeSelector.kt
+++ app/src/main/java/com/example/novaplayer/features/settings/presentation/screen/components/ThemeSelector.kt
@@ -1,29 +1,35 @@
 package com.example.novaplayer.features.settings.presentation.screen.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.novaplayer.R
 import com.example.novaplayer.features.settings.domain.model.ThemeMode
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ThemeSelector(
     selectedTheme: ThemeMode,
@@ -43,54 +49,51 @@ fun ThemeSelector(
         alpha = 0.35f
     )
 
-    ExposedDropdownMenuBox(
-        expanded = expanded,
-        onExpandedChange = {
-            expanded = !expanded
-        },
+    Box(
         modifier = Modifier.fillMaxWidth()
     ) {
 
-        TextField(
-            value = selectedText,
-            onValueChange = {},
-            readOnly = true,
-
-            trailingIcon = {
-                ExposedDropdownMenuDefaults.TrailingIcon(
-                    expanded = expanded
-                )
-            },
-
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(72.dp)
-                .menuAnchor(),
+                .height(48.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(containerColor)
+                .clickable {
+                    expanded = !expanded
+                }
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = selectedText,
+                    modifier = Modifier.weight(1f),
+                    style = MaterialTheme.typography.titleSmall
+                )
 
-            shape = RoundedCornerShape(16.dp),
+                Icon(
+                    imageVector = if (expanded) {
+                        Icons.Default.KeyboardArrowUp
+                    } else {
+                        Icons.Default.KeyboardArrowDown
+                    },
+                    contentDescription = null
+                )
+            }
+        }
 
-            colors = TextFieldDefaults.colors(
-                focusedContainerColor = containerColor,
-                unfocusedContainerColor = containerColor,
-                disabledContainerColor = containerColor,
-
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                disabledIndicatorColor = Color.Transparent
-            ),
-
-            singleLine = true
-        )
-
-        ExposedDropdownMenu(
+        DropdownMenu(
             expanded = expanded,
             onDismissRequest = {
                 expanded = false
             },
-            modifier = Modifier
-                .fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth(0.56f),
+            shape = RoundedCornerShape(16.dp)
         ) {
-
             ThemeMode.entries.forEach { theme ->
 
                 DropdownMenuItem(
