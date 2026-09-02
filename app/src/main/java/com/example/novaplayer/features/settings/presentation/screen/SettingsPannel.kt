35824153e3570984e82a3eacad52bc6d48b5cee6
Ali mahjoob <behnamuiux@gmail.com>
2026-09-03 02:37:45 +0330

    fix bug slide in out in RTL LTR

--- app/src/main/java/com/example/novaplayer/features/settings/presentation/screen/SettingsPannel.kt
+++ app/src/main/java/com/example/novaplayer/features/settings/presentation/screen/SettingsPannel.kt
@@ -1,11 +1,7 @@
 package com.example.novaplayer.features.settings.presentation.screen
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideIn
 import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOut
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -13,68 +9,107 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
 import com.example.novaplayer.features.settings.domain.model.AppLanguage
+import com.kyant.backdrop.backdrops.LayerBackdrop
+import com.kyant.backdrop.drawBackdrop
+import com.kyant.backdrop.effects.blur
+import com.kyant.backdrop.effects.lens
+import com.kyant.backdrop.effects.vibrancy
+
+
 
 @Composable
 fun SettingsPanel(
+    backdrop: LayerBackdrop,
     visible: Boolean,
     onClose: () -> Unit,
     onAboutClick: () -> Unit,
     onLanguageSelected: (AppLanguage) -> Unit
 ) {
+    val isRtl = LocalLayoutDirection.current == LayoutDirection.Rtl
+
+    val color = MaterialTheme.colorScheme.background.copy(alpha = 0.92f)
+
     AnimatedVisibility(
         visible = visible,
+
         enter = slideInHorizontally(
-            initialOffsetX = { it }
+            initialOffsetX = { fullWidth ->
+                if (isRtl) {
+                    -fullWidth
+                } else {
+                    fullWidth
+                }
+            }
         ),
+
         exit = slideOutHorizontally(
-            targetOffsetX = { it }
+            targetOffsetX = { fullWidth ->
+                if (isRtl) {
+                    -fullWidth
+                } else {
+                    fullWidth
+                }
+            }
         )
     ) {
-
-
         Box(
             modifier = Modifier.fillMaxSize()
         ) {
+
+            // Backdrop
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .background(Color.Black.copy(alpha = 0.45f))
                     .clickable {
                         onClose()
                     }
             )
 
-            AnimatedVisibility(
-                visible = true,
-                enter = slideInHorizontally(
-                    initialOffsetX = { fullWidth -> fullWidth }
-                ),
-                exit = slideOutHorizontally(
-                    targetOffsetX = { fullWidth -> fullWidth }
-                ),
-                modifier = Modifier.align(Alignment.CenterEnd)
-            ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .fillMaxWidth(0.68f)
-                        .background(
-                            MaterialTheme.colorScheme.background
-                        )
-                ) {
-                    SettingsScreen(
-                        onClose = onClose,
-                        onAboutClick = onAboutClick,
-                        onLanguageSelected = onLanguageSelected
+            // Settings Panel
+            Box(
+                modifier = Modifier
+                    // پنل همیشه سمت راست است
+                    .align(Alignment.CenterEnd)
+                    .fillMaxHeight()
+                    .fillMaxWidth(0.68f)
+                    .drawBackdrop(
+                        backdrop = backdrop,
+                        shape = {
+                            RoundedCornerShape(16.dp)
+                        },
+                        effects = {
+                            vibrancy()
+                            blur(8f.dp.toPx())
+                            lens(
+                                16f.dp.toPx(),
+                                32f.dp.toPx()
+                            )
+                        },
+                        onDrawSurface = {
+                            drawRect(color)
+                        }
                     )
-                }
+                    .background(Color.Transparent)
+            ) {
+                SettingsScreen(
+                    onClose = onClose,
+                    onAboutClick = onAboutClick,
+                    onLanguageSelected = onLanguageSelected
+                )
             }
         }
     }
 }
+
+
